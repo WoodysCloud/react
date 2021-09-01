ff7b92d1ac292783bfc494bea5937403edeb3fbc
LongSong <wsong15@hawk.iit.edu>
2021-09-01 17:40:15 +0900

    react component finished

---
+++ src/component/TodoFooter.js
@@ -0,0 +1,16 @@
+import footer from '../css/footer.module.css'
+
+function TodoFooter() {
+
+    return(
+        <footer className={footer.footer}>
+            <div className={footer.section}>
+                <div>TODO APP</div>
+                <div>&copy; Copyright All right reserved.</div>
+            </div>
+        </footer>
+    );
+
+}
+
+export default TodoFooter;
