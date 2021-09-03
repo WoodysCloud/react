8cdcb12fe4ebf6cb9f0ff71371b3f6c3d695db95
LongSong <wsong15@hawk.iit.edu>
2021-09-03 14:36:32 +0900

    todo app 완료

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,15 +1,34 @@
 import todolist from '../css/todolist.module.css';
+import todoitem from '../css/todoitem.module.css';
 
-function TodoList({children}) {
+function TodoItem({ todo, removeTodo }) {
+  return (
+    <li className={todoitem['todo-item']}>
+      <input type="checkbox" className={todoitem.check} />
+      <mark className={todoitem.text}>{todo.todoContent}</mark>
+      <button
+        type="button"
+        className={todoitem.del}
+        onClick={function () {
+          return removeTodo(todo.id);
+        }}
+      >
+        <img src="add_button.svg" alt="" className={todoitem.img} />
+      </button>
+    </li>
+  );
+}
 
-    return(
-        <section className={todolist.section}>
-            <ul className="todo-list">
-                {children}
-            </ul>
-        </section>
-    );
-
+function TodoList({ todoArray, removeTodo }) {
+  return (
+    <section className={todolist.section}>
+      <ul className="todo-list">
+        {todoArray.map(function (todo) {
+          return <TodoItem todo={todo} key={todo.id} removeTodo={removeTodo} />;
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default TodoList;
